1cc65bfe5f65a361447e1252fc5b0b0519e5e9a7
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-05-21 13:52:03 -0700

    remaining items

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+URL="https://raw.githubusercontent.com/tnthornton/gvisor-testing/refs/heads/main/gvisor/config.toml"
+
+wget ${URL} -O /k8s-node/config.toml
+cp /install-gvisor.sh /k8s-node
+
+/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/gvisor/install-gvisor.sh
+/usr/bin/nsenter -m/proc/1/ns/mnt /tmp/gvisor/install-gvisor.sh
+/usr/bin/nsenter -m/proc/1/ns/mnt -- cp /etc/containerd/config.toml /etc/containerd/config.toml.org
+/usr/bin/nsenter -m/proc/1/ns/mnt -- cp /tmp/gvisor/config.toml /etc/containerd/config.toml
+/usr/bin/nsenter -m/proc/1/ns/mnt -- systemctl restart containerd
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Successfully installed gvisor and restarted containerd on node ${NODE_NAME}."
+
+sleep infinity
+
